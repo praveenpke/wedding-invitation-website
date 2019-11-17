dd96e9e47c343fcd95b58e62e0dcb1c051b36277
pke <praveenkumar199324@gmail.com>
2019-11-17 16:28:12 -0600

    changes added

---
+++ controller.js
@@ -0,0 +1,32 @@
+const nodemailer = require('nodemailer');
+
+class HandleResponse {
+    async send(req, res) {
+        console.log(req.body);
+
+        let testAccount = await nodemailer.createTestAccount();
+
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'peterpar1230@gmail.com',
+                pass: '1562938007emani'
+            }
+        });
+        const mailOptions = {
+            from: 'peterpar1230@gmail.com',
+            to: 'naveenmaddukuri@gmail.com',
+            subject: `${req.body.username}`,
+            html: `<p>Email: ${req.body.email}</p> <p>Message: ${req.body.message}</p>`
+        };
+        transporter.sendMail(mailOptions, function (err, info) {
+            if(err)
+              console.log(err)
+            else
+              console.log(info);
+         });
+
+    }
+}
+
+module.exports = new HandleResponse();
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const handleResponse = require('./controller');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.post('/submit', (req, res) => {
+    handleResponse.send(req,res).then(()=>{
+        res.send('thank you!');
+    })
+});
+
+app.listen(port, () => console.log(`app listening on port ${port}!`));
