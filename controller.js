10dd6bd6fe4f71db52cc1fed4b884db81a4db8a6
pke <praveenkumar199324@gmail.com>
2019-11-30 14:33:05 -0600

    modified changes

--- controller.js
+++ controller.js
@@ -1,30 +1,32 @@
     const nodemailer = require('nodemailer');
 
-class HandleResponse {
-    async send(req, res) {
-        let testAccount = await nodemailer.createTestAccount();
+    class HandleResponse {
+        async send(req, res) {
+            let testAccount = await nodemailer.createTestAccount();
 
-        var transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'peterpar1230@gmail.com',
-                pass: '1562938007emani'
-            }
-        });
-        const mailOptions = {
-            from: 'peterpar1230@gmail.com',
-            to: 'naveenmaddukuri@gmail.com',
-            subject: `${req.body.username}`,
-            html: `<p>Email: ${req.body.email}</p> <p>Message: ${req.body.message} <p>Decision: ${req.body.decision}</p>`
-        };
-        transporter.sendMail(mailOptions, function (err, info) {
-            if(err)
-              console.log(err)
-            else
-              console.log(info);
-         });
+            var transporter = nodemailer.createTransport({
+                host: 'smtp.gmail.com',
+                port: 465,
+                secure: true,
+                auth: {
+                    user: 'peterpar1230@gmail.com',
+                    pass: '1562938007emani'
+                }
+            });
+            const mailOptions = {
+                from: 'peterpar1230@gmail.com',
+                to: 'naveenmaddukuri@gmail.com',
+                subject: `${req.body.username}`,
+                html: `<p>Email: ${req.body.email}</p> <p>Message: ${req.body.message} <p>Decision: ${req.body.decision}</p>`
+            };
+            transporter.sendMail(mailOptions, function (err, info) {
+                if (err)
+                    console.log(err)
+                else
+                    console.log(info);
+            });
 
+        }
     }
-}
 
-module.exports = new HandleResponse();
+    module.exports = new HandleResponse();
